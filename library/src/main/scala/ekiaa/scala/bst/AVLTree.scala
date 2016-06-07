49287186f6fd2a8460c4db9398b9cca4cc725101
Artem Ekimov <ekimov-artem@ya.ru>
2016-06-07 16:52:41 +0300

    Release balancing

--- library/src/main/scala/ekiaa/scala/bst/AVLTree.scala
+++ library/src/main/scala/ekiaa/scala/bst/AVLTree.scala
@@ -4,14 +4,16 @@ import scala.annotation.tailrec
 
 class AVLTree[K : Ordering, V] {
 
-  var root: Option[AVLTree[K, V]] = None
-  var nodeParent: Option[AVLTree[K, V]] = None
+  private type Node = AVLTree[K, V]
+
+  var root: Option[Node] = None
+  var nodeParent: Option[Node] = None
 
   var leftHeight: Int = 0
   var rightHeight: Int = 0
 
-  var left: Option[AVLTree[K, V]] = None
-  var right: Option[AVLTree[K, V]] = None
+  var left: Option[Node] = None
+  var right: Option[Node] = None
 
   var nodeKey: Option[K] = None
   var nodeValue: Option[V] = None
@@ -27,7 +29,7 @@ class AVLTree[K : Ordering, V] {
       case None =>
         root match {
           case None =>
-            val node = new AVLTree[K, V](this)
+            val node = new Node(this)
             root = Some(node)
             node.insert(key, value)
           case Some(node) =>
@@ -44,10 +46,10 @@ class AVLTree[K : Ordering, V] {
               nodeValue = Some(value)
             } else {
               import Ordered._
-              if (nkey < key) {
+              if (nkey > key) {
                 left match {
                   case None =>
-                    val node = new AVLTree[K, V](this)
+                    val node = new Node(this)
                     left = Some(node)
                     node.insert(key, value)
                   case Some(node) =>
@@ -56,7 +58,7 @@ class AVLTree[K : Ordering, V] {
               } else {
                 right match {
                   case None =>
-                    val node = new AVLTree[K, V](this)
+                    val node = new Node(this)
                     right = Some(node)
                     node.insert(key, value)
                   case Some(node) =>
@@ -87,7 +89,7 @@ class AVLTree[K : Ordering, V] {
               nodeValue
             } else {
               import Ordered._
-              if (nkey < key) {
+              if (nkey > key) {
                 left match {
                   case None =>
                     None
@@ -108,22 +110,119 @@ class AVLTree[K : Ordering, V] {
   }
 
   @tailrec
-  private def up(node: AVLTree[K, V]): Unit = {
+  private def up(node: Node): Unit = {
     nodeParent match {
       case None =>
+        checkHeightDifferenceRule(node)
       case Some(parent) =>
         val height: Int = node.maxHeight
         if (left.contains(node)) {
           leftHeight = height + 1
+          checkLeftRightHeightRule(node)
+          left.foreach(checkHeightDifferenceRule)
         } else {
           if (right.contains(node)) {
             rightHeight = height + 1
+            checkLeftRightHeightRule(node)
+            right.foreach(checkHeightDifferenceRule)
           } else {
             throw new Exception("[up] Unexpected node reference")
           }
         }
         parent.up(this)
     }
+  }
+
+  private def checkLeftRightHeightRule(node: Node): Unit = {
+    nodeParent match {
+      case None =>
+      case Some(_) =>
+        if (left.contains(node)) {
+          if (node.rightHeight > node.leftHeight) {
+            leftRotate(node)
+          }
+        } else {
+          if (right.contains(node)) {
+            if (node.leftHeight > node.rightHeight) {
+              rightRotate(node)
+            }
+          } else {
+            throw new Exception("[checkLeftRightHeightRule] Unexpected node reference")
+          }
+        }
+    }
+  }
+
+  private def checkHeightDifferenceRule(node: Node): Unit = {
+    if (node.leftHeight - node.rightHeight >= 2) {
+      rightRotate(node)
+    } else {
+      if (node.rightHeight - node.leftHeight >= 2) {
+        leftRotate(node)
+      }
+    }
+  }
+
+  private def rightRotate(p: Node): Unit = {
+    nodeParent match {
+      case None =>
+        doRightRotate(p).foreach(q => { root = Some(q) })
+      case Some(_) =>
+        if (left.contains(p)) {
+          doRightRotate(p).foreach(q => { left = Some(q) })
+        } else {
+          if (right.contains(p)) {
+            doRightRotate(p).foreach(q => { right = Some(q) })
+          } else {
+            throw new Exception("[rightRotate] Unexpected node reference")
+          }
+        }
+    }
+  }
+
+  private def doRightRotate(p: Node): Option[Node] = {
+    p.left.map(q => {
+      val bNode: Option[Node] = q.right
+      bNode.foreach(b => { b.nodeParent = Some(p) })
+      p.left = bNode
+      p.nodeParent = Some(q)
+      p.leftHeight -= 1
+      q.right = Some(p)
+      q.nodeParent = Some(this)
+      q.rightHeight += 1
+      q
+    })
+  }
+
+  private def leftRotate(q: Node): Unit = {
+    nodeParent match {
+      case None =>
+        doLeftRotate(q).foreach(p => { root = Some(p) })
+      case Some(_) =>
+        if (left.contains(q)) {
+          doLeftRotate(q).foreach(p => { left = Some(p) })
+        } else {
+          if (right.contains(q)) {
+            doLeftRotate(q).foreach(p => { right = Some(p) })
+          } else {
+            throw new Exception("[leftRotate] Unexpected node reference")
+          }
+        }
+    }
+  }
+
+  private def doLeftRotate(q: Node): Option[Node] = {
+    q.left.map(p => {
+      val bNode: Option[Node] = p.left
+      bNode.foreach(b => { b.nodeParent = Some(q) })
+      q.left = bNode
+      q.nodeParent = Some(q)
+      q.rightHeight -= 1
+      p.right = Some(q)
+      p.nodeParent = Some(this)
+      p.leftHeight += 1
+      p
+    })
   }
 
   private def maxHeight: Int = {
--- library/src/main/scala/ekiaa/scala/bst/examples/Example.scala
+++ library/src/main/scala/ekiaa/scala/bst/examples/Example.scala
@@ -8,19 +8,21 @@ object Example extends App with StrictLogging {
   logger.debug("Start")
 
   val tree = new AVLTree[Int, String]
+
+  val value15 = "value15"
+  tree.insert(15, value15)
   val value10: String = "value10"
   tree.insert(10, value10)
   val value5 = "value5"
   tree.insert(5, value5)
-  val value15 = "value15"
-  tree.insert(15, value15)
+  tree.insert(1, "1")
+  tree.insert(2, "2")
+  tree.insert(3, "3")
+  tree.insert(4, "5")
 
   require(tree.get(5).contains(value5))
   require(tree.get(10).contains(value10))
   require(tree.get(15).contains(value15))
-
-  require(tree.root.get.leftHeight == 1)
-  require(tree.root.get.rightHeight == 1)
 
   logger.debug("Stop")
 
